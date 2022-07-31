a266a6c851be9bd2f3beb528d9f243cb5f1b79e0
17920368 <avictor.zrodriguez@gmail.com>
2022-07-31 14:27:49 -0500

    api-github

--- js/main.js
+++ js/main.js
@@ -2,17 +2,49 @@ const FORM = document.getElementById("form");
 const CONTAINER = document.getElementById("container");
 FORM.addEventListener("submit", (e) => {
   e.preventDefault();
-  const username = search.value;
-  console.log(username);
-  getDataUser(username);
+  const userName = search.value;
+  console.log(userName);
+  getDataUser(userName);
 });
-async function getDataUser(username) {
+async function getDataUser(userName) {
   try {
-    let data = await fetch(`https://api.github.com/users/${username}`);
-    let json = await data.json();
-    console.log(json);
+    let data = await fetch(`https://api.github.com/users/${userName}`);
+    let user = await data.json();
+    console.log(user);
+    showUser(user);
   } catch (error) {
     console.log(error);
   }
 }
-function name(params) {}
+function showUser(user) {
+  let newUser = `<article class="container__card">
+                    <section class="card">
+                    <img class="card__profile" src="${user.avatar_url}" alt="${user.name}" />
+                    <h2 class="card__name">${user.name}</h2>
+                    <p class="card__bio">
+                        ${user.bio}
+                    </p>
+                    </section>
+                    <section class="followers">
+                    <section class="followers__follow">
+                        <p>
+                        <i class="fas fa-user-friends"></i>
+                        <span class="followers__follow--count">${user.followers}</span> followers -
+                        <span class="followers__following--count">${user.following}</span> following
+                        </p>
+                    </section>
+                    </section>
+                    <section class="contact">
+                    <p class="contact__city">
+                        <i class="fas fa-map-marker-alt"></i> ${user.location}
+                    </p>
+                    <p class="contact__email">
+                        <i class="far fa-envelope"></i> ${user.email}
+                    </p>
+                    <p class="contact__page">
+                        <i class="fas fa-link"></i> <a href="${user.blog}" target="_black" class="page">${user.blog}</a>
+                    </p>
+                    </section>
+                </article>`;
+  CONTAINER.innerHTML = newUser;
+}
