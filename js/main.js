9fbf50ab9e507b6c0c4b8d469ffb130ad6aea759
17920368 <avictor.zrodriguez@gmail.com>
2022-07-31 13:47:15 -0500

    api-github

---
+++ js/main.js
@@ -0,0 +1,17 @@
+const FORM = document.getElementById("form");
+const CONTAINER = document.getElementById("container");
+FORM.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const username = search.value;
+  console.log(username);
+  getDataUser(username);
+});
+async function getDataUser(username) {
+  try {
+    let data = await fetch(`https://api.github.com/users/${username}`);
+    let json = await data.json();
+    console.log(json);
+  } catch (error) {
+    console.log(error);
+  }
+}
